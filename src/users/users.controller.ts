e56309a1b96de7e37e2e3fe4fcc43662cdde875a
yujiniii <youjin030700@gmail.com>
2022-09-22 00:03:06 +0900

    feat: 사용자 추가 기능 구현

---
+++ src/users/users.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.usersService.create(createUserDto);
+  }
+
+
+
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm';
+import { RaidRecord } from 'src/raid-records/entities/raid-record.entity';
+import { RedisCacheService } from 'src/cache/redis.service';
+import { HttpService } from '@nestjs/axios';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersReopsitory:Repository<User>,
+    @InjectRepository(RaidRecord)
+    private raidRecordsReopsitory:Repository<RaidRecord>,
+    private readonly httpService:HttpService,
+    private cacheManager: RedisCacheService,
+    ){}
+
+  async create(createUserDto: CreateUserDto) {
+    const {name} = createUserDto
+    const newUser = await this.usersReopsitory.create({
+      name
+    });
+    await this.usersReopsitory.save(newUser)
+    return newUser;
+  }
+
+
+}
+
+
